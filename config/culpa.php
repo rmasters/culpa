844f4000bd8113eef29a20653cd282d3d7426034
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-10-15 14:26:50 +0200

    Updated array notation and phpdoc

--- config/culpa.php
+++ config/culpa.php
@@ -7,37 +7,54 @@
  * @copyright Ross Masters 2013
  * @license MIT
  */
-return array(
-    'users' => array(
-        /**
-         * Uncomment the lines below if you want to overwrite the user source
-         *
-         * @note:
-         * enabling this closure breaks laravel's feature to cache config files
-         * The default (commented) option should work for you if you are using the default Auth provider.
-         * @see: https://github.com/laravel/framework/issues/9625
-         * @return int|null User ID, or null if not authenticated
-         *
-         * 'active_user' => function () {
-         * return Auth::check() ? Auth::user()->id : null;
-         * },
-         */
+return [
+    'users' => [
+        /*
+        |--------------------------------------------------------------------------
+        | Custom User Source
+        |--------------------------------------------------------------------------
+        |
+        | Enabling this closure breaks laravel's feature to cache config files
+        | The default (commented) option should work for you if you are using the default Auth provider.
+        | @see: https://github.com/laravel/framework/issues/9625
+        |
+        | @return int|null User ID, or null if not authenticated
+        |
+        |
 
+        */
+//        'active_user' => function () {
+//            return Auth::check() ? Auth::user()->id : null;
+//        },
 
-        /**
-         * Class name of the user object to relate to
-         * @var string
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | User Model Namespace
+        |--------------------------------------------------------------------------
+        |
+        | Class name of the user object to relate to
+        | @var string
+        |
+        |
+        */
+
         'classname' => 'App\User',
 
+    ],
 
-    ),
-    /**
-     * The default datasource fields
-     */
-    'default_fields' => array(
+    /*
+    |--------------------------------------------------------------------------
+    | Default blameable table columns
+    |--------------------------------------------------------------------------
+    |
+    | Default columns for your blameable fields
+    |
+    |
+    */
+
+    'default_fields' => [
         'created' => 'created_by',
         'updated' => 'updated_by',
         'deleted' => 'deleted_by',
-    )
-);
+    ]
+];
