cd8cfce1aca9b8c5c03c7951b014bb85b092dc41
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-10-15 14:22:00 +0200

    Allow the absence of the current_user config value
    You can now cache your config files again

--- config/culpa.php
+++ config/culpa.php
@@ -7,23 +7,34 @@
  * @copyright Ross Masters 2013
  * @license MIT
  */
-use Illuminate\Support\Facades\Auth;
-
 return array(
     'users' => array(
-        /*
-         * Retrieve the authenticated user's ID
+        /**
+         * Uncomment the lines below if you want to overwrite the user source
+         *
+         * @note:
+         * enabling this closure breaks laravel's feature to cache config files
+         * The default (commented) option should work for you if you are using the default Auth provider.
+         * @see: https://github.com/laravel/framework/issues/9625
          * @return int|null User ID, or null if not authenticated
+         *
+         * 'active_user' => function () {
+         * return Auth::check() ? Auth::user()->id : null;
+         * },
          */
-        'active_user' => function () {
-            return Auth::check() ? Auth::user()->id : null;
-        },
-        /*
+
+
+        /**
          * Class name of the user object to relate to
          * @var string
          */
         'classname' => 'App\User',
+
+
     ),
+    /**
+     * The default datasource fields
+     */
     'default_fields' => array(
         'created' => 'created_by',
         'updated' => 'updated_by',
