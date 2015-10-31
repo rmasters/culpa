ca5f30aab88efde39b07d83ab935fa830bbed634
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-10-31 19:48:24 +0100

    Add schema builder tests

--- tests/Bootstrap/AppConfig.php
+++ tests/Bootstrap/AppConfig.php
@@ -2,7 +2,14 @@
 
 namespace Culpa\Tests\Bootstrap;
 
+use Culpa\Facades\Schema;
+
 return array(
+    'app' => [
+        'aliases' => [
+            'Schema' => Schema::class,
+        ],
+    ],
     'database' => array(
         'default' => 'sqlite',
         'connections' => array(
